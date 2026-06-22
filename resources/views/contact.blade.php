402c5493cbfe0126c90276133d6cb0428f5f9721
dangdihh <muhammaddandi121205@gmail.com>
2026-06-23 01:21:29 +0700

    maps dan kontak

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -100,9 +100,9 @@
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-5 h-5"><path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-2.824-1.635-5.165-3.976-6.79-6.81l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" /></svg>
                 </div>
                 <div class="space-y-1">
-                    <h4 class="text-xs font-bold text-slate-900 font-headline">Telepon Kantor</h4>
+                    <h4 class="text-xs font-bold text-slate-900 font-headline">Telepon Sekolahr</h4>
                     <p class="text-[11px] text-slate-400 leading-normal font-mono">
-                        (021) 7301234 <br><span class="text-slate-300">ext. 101 (Informasi PPDB)</span>
+                        0895 3458 77321 <br><span class="text-slate-300">(Untuk Informasi PPDB)</span>
                     </p>
                 </div>
             </div>
@@ -124,7 +124,7 @@
         <section class="max-w-7xl mx-auto px-6 lg:px-8 mb-8">
             <div class="relative bg-teal-900/5 rounded-3xl overflow-hidden border border-slate-200 shadow-xs aspect-[21/9] min-h-[220px]">
                 <iframe
-                    src="https://maps.app.goo.gl/t9J5hWaypgizm4K98"
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3965.469570711995!2d106.71861957462846!3d-6.3331607936564644!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69e57d2d5a24cd%3A0xfe3db724491e5210!2sSD%20Negeri%20Ciledug%20Barat!5e0!3m2!1sid!2sid!4v1782152215374!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"
                     class="absolute inset-0 w-full h-full border-0 grayscale hover:grayscale-0 transition-all duration-700 opacity-90"
                     allowfullscreen=""
                     loading="lazy"
